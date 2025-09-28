6bb55efa8d0eefe7cf6834c2abc3c6cd3d681168
Sagar Singh <sagarsingh@Sagars-MacBook-Pro.local>
2025-09-28 13:21:29 +0530

    prod backend link added

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "https://pharma-sales-backend-v1.onrender.com/",
 //   withCredentials: true,
 });
 
